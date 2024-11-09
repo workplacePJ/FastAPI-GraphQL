d9e60b0618ea15f22abe545a83bcd2d4e32eb112
workplacePJ <workplace.pj@proton.me>
2024-11-09 15:44:56 +0900

    Update query.py

--- api/schemas/query.py
+++ api/schemas/query.py
@@ -1,7 +1,8 @@
 import strawberry
+from company import Company
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def company(self) -> Company:
+    async def company(self) -> Company:
         return Company(id = 1, name = '株式会社トータルメディエイト', postalCode = '1140034', address = '東京都北区上十条2-25-4')
