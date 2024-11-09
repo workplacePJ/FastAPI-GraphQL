a2a0850451950e91664b6736f96c7aac12770dbe
workplacePJ <workplace.pj@proton.me>
2024-11-09 16:20:54 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
 import strawberry
 from strawberry.fastapi import GraphQLRouter
-from schemas.company import Company
+from schemas.query import Query
 import nest_asyncio
 nest_asyncio.apply()
 
-@strawberry.type
-class Query:
-    @strawberry.field
-    def company(self) -> Company:
-        return Company(name = '株式会社トータルメディエイト', postalCode = '1140034', address = '東京都北区上十条2-25-4')
-
 schema = strawberry.Schema(Query)
-
 graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
