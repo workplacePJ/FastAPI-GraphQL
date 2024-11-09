2046fa94cc4041d073632bddf5eff595d6649901
workplacePJ <workplace.pj@proton.me>
2024-11-09 14:28:48 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -17,32 +17,3 @@ graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
 app.include_router(graphql_app, prefix = '/graphql')
-
-
-
-
-
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-from pyngrok import ngrok
-
-
-# Add the CORSMiddleware immediately after creating the app
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = ['*'],
-    allow_credentials = True,
-    allow_methods = ['*'],
-    allow_headers = ['*'],
-)
-
-
-from google.colab import userdata
-auth_token = userdata.get('ngrok')
-ngrok.set_auth_token(auth_token)
-
-ngrok_tunnel = ngrok.connect(8000)
-print('PUBLIC_URL:', ngrok_tunnel.public_url)
-print('PUBLIC_URL_PREFIX:', f'{ngrok_tunnel.public_url}/graphql')
-print('PUBLIC_DOCS_URL:', f'{ngrok_tunnel.public_url}/docs')
-uvicorn.run(app, port = 8000)
