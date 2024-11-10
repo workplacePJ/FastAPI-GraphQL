fa2a768e40c95e431ed381f33d21d9351607960b
workplacePJ <workplace.pj@proton.me>
2024-11-11 05:44:23 +0900

    Update resolvers.py

--- src/api/ owner/resolvers.py
+++ src/api/ owner/resolvers.py
@@ -2,7 +2,7 @@ import strawberry
 from type import Owner
 from strawberry.types import Info
 
-async def owner(info: Info, id: int) -> Owner:
+async def owner_get(info: Info, id: int) -> Owner:
     #owner_dict = await get_owner(info, id)
     owner_dict = Owner(id = 1, name = '鈴木一郎', postalCode = '1140034', address = '東京都北区上十条2-25-4')
     return owner_dict
