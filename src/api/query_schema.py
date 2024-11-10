f6e6d4b36ec05e8eecbbc6d297137b57d5a0482d
workplacePJ <workplace.pj@proton.me>
2024-11-10 23:15:56 +0900

    Update query_schema.py

--- src/api/query_schema.py
+++ src/api/query_schema.py
@@ -1,4 +1,19 @@
 import strawberry
 
+#@strawberry.type
+#class Query:
+
+
+
+@strawberry.type
+class Owner:
+    id: int
+    name: str
+    postalCode: str
+    address: str
+
 @strawberry.type
 class Query:
+    @strawberry.field
+    async def owner(self) -> Owner:
+        return Owner(id = 1, name = '株式会社トータルメディエイト', postalCode = '1140034', address = '東京都北区上十条2-25-4')
