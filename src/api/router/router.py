b83dca946c6c03811c2bf36168785afea125b45d
workplacePJ <workplace.pj@proton.me>
2024-11-10 17:39:47 +0900

    Update router.py

--- src/api/router/router.py
+++ src/api/router/router.py
@@ -29,6 +29,3 @@ task_app = GraphQLRouter(schema, context_getter=get_context)
 
 schema = strawberry.Schema(Query)
 graphql_app = GraphQLRouter(schema)
-
-app = FastAPI()
-app.include_router(graphql_app, prefix = '/graphql')
