bb1cf3ca2c8af7a58b7d5b2863a7ab1804d3d800
workplacePJ <workplace.pj@proton.me>
2024-11-11 04:22:36 +0900

    Update resolvers.py

--- src/api/ owner/resolvers.py
+++ src/api/ owner/resolvers.py
@@ -1,1 +1,6 @@
+import strawberry
+from type import Owner
+from strawberry.types import Info
 
+async def get_owner(info: Info) -> Owner:
+    return Owner(id = 1, name = '株式会社トータルメディエイト', postalCode = '1140034', address = '東京都北区上十条2-25-4')
