65b03666b6eb4fa91557b30d4aa5522caca3e43d
workplacePJ <workplace.pj@proton.me>
2024-11-11 03:58:15 +0900

    Update query_schema.py

--- src/api/query_schema.py
+++ src/api/query_schema.py
@@ -1,9 +1,6 @@
 import strawberry
 from owner.types import Owner
 
-async def get_owner(self) -> Owner:
-    return Owner(id = 1, name = '株式会社トータルメディエイト', postalCode = '1140034', address = '東京都北区上十条2-25-4')
-
 @strawberry.type
 class Query:
     owner: Owner = strawberry.field(resolver = get_owner)
