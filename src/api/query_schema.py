577d49e6c9087d22f02fb11b01afe798eff42f89
workplacePJ <workplace.pj@proton.me>
2024-11-11 06:04:34 +0900

    Update query_schema.py

--- src/api/query_schema.py
+++ src/api/query_schema.py
@@ -1,6 +1,11 @@
 import strawberry
-from .owner.type import Owner
-from .owner.resolvers import owner_get
+from owner.type import Owner
+#from .owner.resolvers import owner_get
+
+async def owner_get(info: Info, id: int) -> Owner:
+    #owner_dict = await get_owner(info, id)
+    owner_dict = Owner(id = 1, name = '鈴木一郎', postalCode = '1140034', address = '東京都北区上十条2-25-4')
+    return owner_dict
 
 @strawberry.type
 class Query:
