f4c6d151295015ced753bc5ddd34dd3eda726658
workplacePJ <workplace.pj@proton.me>
2024-11-11 07:58:40 +0900

    Delete api directory

--- api/schemas/company.py
+++
@@ -1,8 +0,0 @@
-import strawberry
-
-@strawberry.type
-class Company:
-    id: int
-    name: str
-    postalCode: str
-    address: str
--- api/schemas/query.py
+++
@@ -1,8 +0,0 @@
-import strawberry
-from .company import Company
-
-@strawberry.type
-class Query:
-    @strawberry.field
-    async def company(self) -> Company:
-        return Company(id = 1, name = '株式会社トータルメディエイト', postalCode = '1140034', address = '東京都北区上十条2-25-4')
