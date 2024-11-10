d2fccf08bb6aff0b4e4f35140867eaa0ba879940
workplacePJ <workplace.pj@proton.me>
2024-11-11 05:27:49 +0900

    Update query_schema.py

--- src/api/query_schema.py
+++ src/api/query_schema.py
@@ -1,6 +1,6 @@
 import strawberry
-from .owner.type import Owner
-from .owner.resolvers import owner
+from owner.type import Owner
+from owner.resolvers import owner
 
 @strawberry.type
 class Query:
