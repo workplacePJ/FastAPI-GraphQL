450c01ab8229b843d351d4ed5de5ad1d7dd5c66b
workplacePJ <workplace.pj@proton.me>
2024-11-11 12:22:20 +0900

    Update type.py

--- src/api/owner/type.py
+++ src/api/owner/type.py
@@ -4,5 +4,27 @@ import strawberry
 class Owner:
     id: int
     name: str
-    postalCode: str
+    nameKana: str = Field(..., pattern = r'^[ァ-ヴー]+$')
+    postalCode: str = Field(..., pattern = r'^[0-9]{7}$')
     address: str
+    buildingName: str | None = None
+    roomNumber: str | None = None
+    tel: str = Field(..., pattern = r'^[0-9]{10,11}$')
+    mail: str | None = Field(None, pattern = r'^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$')
+    #bankAccount: 
+    remarks: str | None = None
+    creatAt:
+    
+
+class CompanyBase(BaseModel):
+    name: str
+    postal_code: str = Field(..., pattern = r'^[0-9]{7}$')
+    address: str
+    building_name: str | None = None
+    room_number: str | None = None
+    tel: str
+    fax: str
+    #mail: str = Field(..., pattern = r'^$')
+    #HP: str = Field(..., pattern = r'^$')
+    #invoice_registration_number: str = Field(..., pattern = r'^$')
+    #company_seal: str = Field(..., pattern = r'^$')
