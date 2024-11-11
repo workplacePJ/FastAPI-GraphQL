ccb631d66fbfb2e155549e6c2b9d8cde70f821e2
workplacePJ <workplace.pj@proton.me>
2024-11-11 16:34:21 +0900

    Update resolvers.py

--- src/api/owner/resolvers.py
+++ src/api/owner/resolvers.py
@@ -5,11 +5,53 @@ from strawberry.types import Info
 async def get_data__owner(info: Info, id: int) -> Owner:
     """Get owner by id"""
     #owner_dict = await get_dict__owner(info, id)
-    owner_dict = Owner(info, id = 1, name = '鈴木一郎', postalCode = '1140034', address = '東京都北区上十条2-25-4')
+    owner_dict = Owner(
+        id = 1,
+        name = '鈴木一郎',
+        name_kana = 'スズキイチロウ',
+        postal_code = '1142345',
+        address = '東京都板橋区赤坂1-32-12',
+        address_building = null,
+        address_room_number = null,
+        tel = '09011111111',
+        mail = 'info@jsx.com',
+        remarks = null,
+        created_at = datetime.now(),
+        updated_at = datetime.now()
+    )
     return owner_dict
 
 async def get_data__owners(info: Info) -> list[Owner]:
     """Get all owners"""
     #owners_dict_list = await get_dict__owners(info)
-    owners_dict_list = [Owner(id = 1, name = '鈴木一郎', postalCode = '1140034', address = '東京都北区上十条2-25-4'), Owner(id = 2, name = '鈴木二郎', postalCode = '1150056', address = '東京都北区赤羽2-25-4')]
+    owners_dict_list = [
+        Owner(
+            id = 1,
+            name = '鈴木一郎',
+            name_kana = 'スズキイチロウ',
+            postal_code = '1142345',
+            address = '東京都板橋区赤坂1-32-12',
+            address_building = null,
+            address_room_number = null,
+            tel = '09011111111',
+            mail = 'info@jsx.com',
+            remarks = null,
+            created_at = datetime.now(),
+            updated_at = datetime.now()
+        ), 
+        Owner(
+            id = 2,
+            name = '鈴木二郎',
+            name_kana = 'スズキジロウ',
+            postal_code = '1142345',
+            address = '東京都板橋区赤坂1-32-12',
+            address_building = null,
+            address_room_number = null,
+            tel = '09022222222',
+            mail = 'info@jsx2.com',
+            remarks = null,
+            created_at = datetime.now(),
+            updated_at = datetime.now()
+        )
+    ]
     return owners_dict_list
