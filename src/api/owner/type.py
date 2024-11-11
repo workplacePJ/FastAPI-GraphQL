47f7ccdcd32d2c5be7fbd65045c2f305dd7c21d9
workplacePJ <workplace.pj@proton.me>
2024-11-11 16:48:02 +0900

    Update type.py

--- src/api/owner/type.py
+++ src/api/owner/type.py
@@ -6,13 +6,13 @@ from strawberry import field
 class Owner:
     id: int
     name: str
-    name_kana: str = Field(..., pattern = r'^[ァ-ヴー]+$')
-    postal_code: str = Field(..., pattern = r'^[0-9]{7}$')
+    name_kana: str = field(..., pattern = r'^[ァ-ヴー]+$')
+    postal_code: str = field(..., pattern = r'^[0-9]{7}$')
     address: str
     address_building: str | None = None
     address_room_number: str | None = None
-    tel: str = Field(..., pattern = r'^[0-9]{10,11}$')
-    mail: str | None = Field(None, pattern = r'^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$')
+    tel: str = field(..., pattern = r'^[0-9]{10,11}$')
+    mail: str | None = field(None, pattern = r'^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$')
     #bank_account: 
     remarks: str | None = None
     created_at: datetime
