7637b85e0768fc43a4a09474e7701962c7e4e0c4
workplacePJ <workplace.pj@proton.me>
2024-11-11 15:34:30 +0900

    Update type.py

--- src/api/owner/type.py
+++ src/api/owner/type.py
@@ -1,4 +1,5 @@
 import strawberry
+from datetime import datetime
 
 @strawberry.type
 class Owner:
@@ -13,18 +14,5 @@ class Owner:
     mail: str | None = Field(None, pattern = r'^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$')
     #bankAccount: 
     remarks: str | None = None
-    creatAt:
-    
-
-class CompanyBase(BaseModel):
-    name: str
-    postal_code: str = Field(..., pattern = r'^[0-9]{7}$')
-    address: str
-    building_name: str | None = None
-    room_number: str | None = None
-    tel: str
-    fax: str
-    #mail: str = Field(..., pattern = r'^$')
-    #HP: str = Field(..., pattern = r'^$')
-    #invoice_registration_number: str = Field(..., pattern = r'^$')
-    #company_seal: str = Field(..., pattern = r'^$')
+    creatrdAt: datetime
+    updatedAt: datetime
