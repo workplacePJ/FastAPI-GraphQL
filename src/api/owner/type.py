a50f556c6e22fe795edbcf2ed152c60d9d67965a
workplacePJ <workplace.pj@proton.me>
2024-11-11 17:53:26 +0900

    Update type.py

--- src/api/owner/type.py
+++ src/api/owner/type.py
@@ -1,7 +1,25 @@
 import strawberry
 from datetime import datetime
-from strawberry import field
 
+@strawberry.type
+class Owner:
+    id: int
+    name: str
+    name_kana: str
+    postal_code: str
+    address: str
+    address_building: str | None = None
+    address_room_number_or_floor: str | None = None
+    tel: str
+    mail: str | None = None
+    #bank_account: 
+    remarks: str | None = None
+    created_at: datetime
+    updated_at: datetime
+
+
+
+"""
 @strawberry.type
 class Owner:
     id: int
@@ -10,10 +28,11 @@ class Owner:
     postal_code: str = field(..., pattern = r'^[0-9]{7}$')
     address: str
     address_building: str | None = None
-    address_room_number: str | None = None
+    address_room_number_or_floor: str | None = None
     tel: str = field(..., pattern = r'^[0-9]{10,11}$')
     mail: str | None = field(None, pattern = r'^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$')
     #bank_account: 
     remarks: str | None = None
     created_at: datetime
     updated_at: datetime
+"""
