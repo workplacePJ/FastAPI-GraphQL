2fc9a818499765c28b61ff12a8285ca45156572c
workplacePJ <workplace.pj@proton.me>
2024-11-11 16:06:45 +0900

    Update router.py

--- src/api/router.py
+++ src/api/router.py
@@ -1,8 +1,9 @@
 import strawberry
 from strawberry.fastapi import GraphQLRouter
+from strawberry.schema.config import StrawberryConfig
 from query_schema import Query
 #from mutation_schema import Mutation
 
-schema = strawberry.Schema(query = Query)
-#schema = strawberry.Schema(query = Query, mutation = Mutation)
+schema = strawberry.Schema(query = Query, config = StrawberryConfig(auto_camel_case = True))
+#schema = strawberry.Schema(query = Query, mutation = Mutation, config = StrawberryConfig(auto_camel_case = True))
 graphql_app = GraphQLRouter(schema)
