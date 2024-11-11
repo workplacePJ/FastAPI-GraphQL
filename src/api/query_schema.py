4592dd7a9e626f471510d27df41a5988658e0264
workplacePJ <workplace.pj@proton.me>
2024-11-11 10:41:00 +0900

    Update query_schema.py

--- src/api/query_schema.py
+++ src/api/query_schema.py
@@ -1,7 +1,8 @@
 import strawberry
 from owner.type import Owner
-from owner.resolvers import get_data__owner
+from owner.resolvers import get_data__owner, get_data__owners
 
 @strawberry.type
 class Query:
     owner: Owner = strawberry.field(resolver = get_data__owner)
+    owner: Owners = strawberry.field(resolver = get_data__owners)
